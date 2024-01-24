f5738ae2198f88ab2f8b7ed8456ef281ef7c8abe
james <emperor2g@gmail.com>
2024-01-24 23:53:03 +0100

    carousel

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,31 +1,149 @@
-import React, { useState, useEffect } from 'react'
-import { TrendingCoins } from '../../config/api'
-import axios from 'axios'
-import {CryptoState} from "../../CryptoContext"
-import {} from '../../config/api'
+// import React, { useState, useEffect } from 'react'
+// import { TrendingCoins } from '../../config/api'
+// import axios from 'axios'
+// import {CryptoState} from "../../CryptoContext"
+// import {} from '../../config/api'
+// import AliceCarousel from 'react-alice-carousel'
+// import { Link } from 'react-router-dom'
 
+
+// const Carousel = () => {
+//   const [trending, setTrending] = useState([]);
+//   const { currency } = CryptoState()
+//   const fetchTrendingCoins =async () => {
+//     const { data } = await axios.get(TrendingCoins(currency));
+//     setTrending(data);
+//     console.log(trending);
+//   };
+
+// useEffect(() => {
+//   fetchTrendingCoins();
+// }, [currency]);
+
+//     const items = trending.map((coin)=> {
+
+//       return (
+//         <Link
+
+//         to = {`/coins/${coin.id}`}>
+//         </Link>
+//       )
+//     })
+
+// const responsive = {
+//   0:{
+//     items: 2,
+//   },
+//   512:{
+//     items: 4,
+//   },
+// };
+
+//   return (
+    
+//     <div className='carousel'>
+
+//      <AliceCarousel
+//      mouseTracking
+//      infinite
+//      autoPlayInterval={1000}
+//      animationDuration={1500}
+//      disableDotsControls
+//      responsive = {responsive}
+//      autoPlay
+//      items={items}
+//      />
+     
+
+      
+//     </div>
+//   )
+// }
+
+// export default Carousel
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins} from "../../config/api";
+import { CryptoState } from "../../CryptoContext";
+// import { numberWithCommas } from "../CoinsTable";
 
 const Carousel = () => {
-  const [trending, setTrending] = useState([]);
-  const { currency } = CryptoState()
-  const fetchTrendingCoins =async () => {
+ const [trending, setTrending] = useState([]);
+ const { currency } = CryptoState();
+
+ const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
+
+    console.log(data);
     setTrending(data);
-    console.log(trending);
-  };
+ };
 
-useEffect(() => {
-  fetchTrendingCoins();
-}, [currency]);
+ useEffect(() => {
+    fetchTrendingCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+ }, [currency]);
 
-  return (
-    
-    <div className='carousel'>
+ const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
 
-     Carousel
-      
+    return (
+      <Link to={`/coins/${coin.id}`}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", cursor: "pointer", textTransform: "uppercase", color: "white" }}>
+          <img
+            src={coin?.image}
+            alt={coin.name}
+            height="80"
+            style={{ marginBottom: 10 }}
+          />
+          <span>
+            {coin?.symbol}
+            &nbsp;
+            <span
+              style={{
+                color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                fontWeight: 500,
+              }}
+            >
+              {profit && "+"}
+              {coin?.price_change_percentage_24h?.toFixed(2)}%
+            </span>
+          </span>
+          <span style={{ fontSize: 22, fontWeight: 500 }}>
+             {/* {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}  */}
+          </span>
+        </div>
+      </Link>
+    );
+ });
+
+ const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+ };
+
+ return (
+    <div style={{ height: "50%", display: "flex", alignItems: "center" }}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+        autoPlay
+      />
     </div>
-  )
-}
+ );
+};
 
-export default Carousel
+export default Carousel;
